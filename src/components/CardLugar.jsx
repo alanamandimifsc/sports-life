73161d1121f9fd05161648fd2f5bc2bc9b1bbdad
Alana IFSC <alana.brm@aluno.ifsc.edu.br>
2024-04-26 20:57:58 -0300

    Iniciado protótipo da página lista locais

--- src/components/CardLugar.jsx
+++ src/components/CardLugar.jsx
@@ -1,10 +1,29 @@
 import { Link } from "react-router-dom";
+import React, { useEffect } from 'react'
 export function CardLugar() {
+
+    const [places, setPlaces] = React.useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:3000/lugares')
+            .then(response => response.json())
+            .then(data => setPlaces(data))
+            .catch(error => console.log('Erro ao buscar lugares:', error))
+    }, [])
+
     return (
         <div className="card">
-            <h2>Nome do lugar</h2>
-            <p>Descrição do lugar</p>
-            <Link to="/registerPlace">Editar</Link>
+            {!!places && places.map(place => (
+                <div key={place.id}>
+                    <h3 >{place.nome}</h3>
+                    <p>{place.descricao}</p>
+                    <p>Criado pelo usuario : {place.id_usuario}</p>
+                    <p>Localização</p>
+                    <p>Rua: {place.rua}<span> número: {place.numero}</span> </p>
+                    <Link to={`/registerPlace/${place.id}`}>Editar</Link>
+                </div>
+            ))}
+
         </div>
     )
 }
--- src/pages/PlaceList.jsx
+++ src/pages/PlaceList.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { CardLugar } from '../components/CardLugar'
+
 export const PlaceList = () => {
+    // const [places, setPlaces] = React.useState([])
+
+    // useEffect(() => {
+    //     fetch('http://localhost:3000/lugares')
+    //         .then(response => response.json())
+    //         .then(data => setPlaces(data))
+    //         .catch(error => console.log('Erro ao buscar lugares:', error))
+    // }, [])
+
     return (
         <div>
             <h1>Conheça as melhores dicas de lugar!</h1>
             <CardLugar />
+
+
 
         </div>
     )
